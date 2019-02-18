0734cd71844091c5e1556e4c5eb4173e464bd14d
Teb Mosby <tibbsr09@gmail.com>
2019-02-17 21:47:06 -0500

    cleaned up JS page & started to form my function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,44 @@
+// Variable:
+// Array
 var teams = [
-    "Liverpool" ||
-    "Real Madrid" ||
-    "Barcelona" ||
-    "Juventus" ||
-    "Manchester City" ||
-    "Paris Saint Germain" ||
-    "Borussia Dortmund" ||
-    "AS Roma" ||
-    "Tottenham" ||
-    "Ajax" ||
-    "Napoli" ||
-    "Inter Milan" ||
-    "Lyon" ||
+    "Liverpool",
+    "Real Madrid",
+    "Barcelona",
+    "Juventus",
+    "Manchester City",
+    "Paris Saint Germain",
+    "Borussia Dortmund",
+    "AS Roma",
+    "Tottenham",
+    "Ajax",
+    "Napoli",
+    "Inter Milan",
+    "Lyon" 
     ];
+// Visual Variables
+var wins = 0;
+var guesses = 12;
+var lettersUsed = 0;
+var userGuess = event.key;
+// 
+
+
+// All console logs:
+console.log(teams.length);
+console.log(teams[0]);
+console.log(teams[1]);
+console.log(teams[2]);
+console.log(teams[3]);
+console.log(teams[4]);
+console.log(teams[5]);
+console.log(teams[8]);
+console.log(teams[10]);
+console.log(teams[11]);
+console.log(teams[12]);
+
+// Functions
+// 
+document.onkeyup = function(event){
 
 var team = teams[Math.floor(Math.random()*teams.length)];
 
@@ -22,3 +48,14 @@ for (var i= 0; i < team.length; i++) {
 }
 
 var remainingLetters = team.length;
+
+for(var t = 0; t < team.length; t++) {
+    if(team[t] === guess) {
+        answerArry[t] = guess;
+        remainingLetters--;
+    }
+}
+}
+
+
+// User Display
