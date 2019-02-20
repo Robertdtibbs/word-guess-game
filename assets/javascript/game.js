c44c08b2cec243e6b9efe54a74b4457591ca4a1c
Teb Mosby <tibbsr09@gmail.com>
2019-02-19 22:19:29 -0500

    added cleaner var and established my textContent links to work my functions off of

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // Variable:
 // Array
-var teams = [
+var europeTeams = [
     "Liverpool",
     "Real Madrid",
     "Barcelona",
@@ -16,12 +16,6 @@ var teams = [
     "Lyon" 
     ];
 // More Variables
-var wins = 0;
-var guessesLeft = 12;
-var lettersPressed = [];
-var userGuess = [];
-// 
-
 
 // All console logs:
 console.log(teams.length);
@@ -36,6 +30,22 @@ console.log(teams[10]);
 console.log(teams[11]);
 console.log(teams[12]);
 
+
+var guessesLeft = 12;
+
+var championsLeague = {    
+    guessingWord = [],
+    guessedLetters = [],
+    wins = 0,
+    teams = europeTeams,
+}
+
+// var userChoiceText = document.getElementById("current-word");
+var winsText = document.getElementById("total-wins");
+var computerChoice = document.getElementById("current-word");
+var guessText = document.getElementById("remaining-guesses");
+var chosenText = document.getElementById("letters-chosen");
+
 // Functions
 // starts function when key is pressed.
 document.onkeyup = function(event){
@@ -43,24 +53,29 @@ document.onkeyup = function(event){
     var userGuess = event.key;
 
     //  Randomly chooses a team name from array.
-    var team = teams[Math.floor(Math.random()*teams.length)];
+    var randomTeam = teams[Math.floor(Math.random()*teams.length)];
 
-    var answerArray = [];
+    userGuess= [];
+    // Sets template of blank spaces for the hangman word the user must guess.
     for (var i= 0; i < team.length; i++) {
-        answerArry[i] = "_";
+        userGuess.push("_");
     }
 
-    var remainingLetters = team.length;
+    if(event.key === "A", "B", "C") {
+        letterGuess(event.key.toLowerCase());
+    }
 
-    for(var t = 0; t < team.length; t++) {
-        if(team[t] === guess) {
-            answerArry[t] = guess;
-            remainingLetters--;
+    function letterGuess(letter) {
+        if (userGuess.indexOf(letter) === -1) {
+            userGuess.push(letter);
+            evaluateLetter(letter);
         }
     }
-    // User Display
-
+    // functiont to search for letter in 
 }
 
-
-
+// code to push outputs to HTML
+winsText.textContent = wins;
+computerChoice.textContent = "";
+guessText.textContent = remainingGuesses;
+chosenText.textContent = "";
