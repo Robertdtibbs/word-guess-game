12a72505af16dbacfa1f23844174dae18ea74ab0
Teb Mosby <tibbsr09@gmail.com>
2019-02-26 19:53:00 -0500

    fine tunning complete - enjoy the game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,9 +37,50 @@ console.log(teams[12]);
 // Functions
 //  Randomly chooses a team name from array.
 var startUp = function() {
+    
     for (var i= 0; i < guessingWord.length; i++) {
         display[i] = "_ ";
         output = output + display[i];
+    }
+    document.getElementById("current-word").innerHTML = output;
+    document.getElementById("remainingGuesses").innerHTML = guessesLeft;
+    output = "";
+    document.getElementById("total-wins").innerHTML = "Wins: " + userWins;
+
+}
+
+var startUpReset = function() {
+    teams = [
+        "Liverpool",
+        "RealMadrid",
+        "Barcelona",
+        "Juventus",
+        "ManchesterCity",
+        "PSG",
+        "Dortmund",
+        "ASRoma",
+        "Tottenham",
+        "Ajax",
+        "Napoli",
+        "Milan",
+        "Lyon" 
+        ];
+    randomTeams = Math.floor(Math.random()*teams.length);
+    guessesLeft = 12;
+    guessingWord = teams[randomTeams];
+    guessedLetters = guessingWord.length;
+    display = [guessedLetters];
+    win = 0;
+    letters = guessingWord.split('');
+    output = "";
+    userLetter = "";
+    letterUsed = [];
+    correctLetter = 0;
+    
+    for (var i= 0; i < guessingWord.length; i++) {
+        display[i] = "_ ";
+        output = output + display[i];
+
     }
     document.getElementById("current-word").innerHTML = output;
     document.getElementById("remainingGuesses").innerHTML = guessesLeft;
@@ -79,44 +120,21 @@ document.onkeyup = function(event){
         } 
     }
 
-    // if(keyCode >= 65 && keyCode <= 90) {
-    //     if (letterUsed.indexOf(userLetter) === -1) {
-    //         letterUsed.push(userLetter);
-    //         for (var c = 0; c < guessingWord.length; c++){
-           
-    //             console.log(c)
-    //             console.log(userLetter)
-    //             console.log(guessingWord[c])
-    //             if(userLetter.toLowerCase() === guessingWord[c].toLowerCase()) {
-    //                 letterMatch = true;
-    //                 display[c] = userLetter.toUpperCase();
-    //                 win++;
-    //                 console.log(win + "win") 
-    //             }
-                    
-    //         }
-    //         output = output + display[c] + " ";
-    //         document.getElementById("lettersChosen").innerHTML = letterUsed.join(", ");
-    //     }
-    //     if(!letterMatch){
-    //         guessesLeft--;
-    //     } 
-    // }
-
-
     document.getElementById("current-word").innerHTML = output;
     output = "";
     
-
     console.log(win, guessingWord.length)
     if(win === guessingWord.length)
     {
         ++userWins;
         alert("Champions of Europe");
-        startUp()
+        startUpReset()
+       
     }
     else if (guessesLeft < 1){
         alert("Off to Europa League");
+        startUpReset();
+        
     }
     else {
         document.getElementById("remainingGuesses").innerHTML = guessesLeft;
